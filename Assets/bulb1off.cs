bd6d46918972a46258be65f905014cdfca77723f
SuryaKumar-IoT <orl.suryakumar@gmail.com>
2020-10-22 19:35:18 +0530

    AR Smart Home Unity

---
+++ Assets/bulb1off.cs
@@ -0,0 +1,38 @@
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Text;
+using UnityEngine;
+
+public class bulb1off : MonoBehaviour
+{
+    public Button yourButton;
+    public Text displaytext;
+
+    void Start ()
+    {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(task);
+    }
+    void task()
+    {
+	StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+    	
+	UnityWebRequest www = UnityWebRequest.Get("http://madblocks.tech/dashboard/device_push.php?device_api_key=602b11493d6a19159cb8eb7809bd80e8&device_status=off1");
+     yield return www.SendWebRequest();
+         
+         if(www.isNetworkError || www.isHttpError)
+         {
+            Debug.Log(www.error);
+         }
+         else
+         {
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+         }
+     }
+}
